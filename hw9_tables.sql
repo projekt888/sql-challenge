afe8e4580056524fe6444d3a77d162feb947bcdf
projekt888 <roberts888@gmail.com>
2019-10-05 17:11:30 -0400

    well

--- hw9_tables.sql
+++ hw9_tables.sql
@@ -39,3 +39,22 @@ CREATE TABLE Employees (
     gender char   NOT NULL,
     hire_date date   NOT NULL
 );
+
+/*1. List the following details of each employee: employee number, last name, first name, gender, and salary.*/
+SELECT Employees.emp_no, Employees.last_name, Employees.first_name, Employees.gender, Salaries.salary FROM Salaries INNER JOIN Employees ON Employees.emp_no=Salaries.emp_no
+
+/*2. List employees who were hired in 1986.*/
+SELECT * FROM Employees WHERE EXTRACT(year FROM "hire_date") = 1986
+
+/*3. List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name, and start and end employment dates.*/
+SELECT Dept_Manager.dept_no, Departments.dept_name, Dept_Manager.emp_no FROM Departments INNER JOIN Dept_Manager ON Dept_Manager.dept_no=Departments.dept_no;
+
+/*4. List the department of each employee with the following information: employee number, last name, first name, and department name.
+
+5. List all employees whose first name is "Hercules" and last names begin with "B."
+
+6. List all employees in the Sales department, including their employee number, last name, first name, and department name.
+
+7. List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+
+8. In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.*/
